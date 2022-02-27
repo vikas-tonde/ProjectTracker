9b38f36c09775f7dcbe80880e40a70bf182b7b2f
Vikas <tondev23@gmail.com>
2022-02-27 13:49:07 +0530

    Deleted Role and RoleController

--- src/main/resources/db/migration/V2__initTables.sql
+++ src/main/resources/db/migration/V2__initTables.sql
@@ -1,7 +1,4 @@
-# create table Role (roleId integer primary key, role varchar(20));
-#
-# insert into Role values(1,'Project Manager');
-# insert into Role values(1,'Employee');
+
 
 create table User (
     u_id integer primary key,
@@ -15,5 +12,3 @@ create table User (
 );
 
 insert into User values (1,'user', 'user@mail.com', '123456', 'pune', '9087654321','1998-06-21','Project Manager')
-
-# create table UserRole (roleId integer references Role(roleId), uID integer references User(uId));
